5d920347fc6f78ee82df305029fda7407efec97c
Sophia Li <sophiali124@gmail.com>
2020-06-11 19:08:22 -0700

    fix tests

--- script.js
+++ script.js
@@ -95,5 +95,3 @@ function handleRestart() {
   cells.forEach((i) => i.addEventListener('click', handleCellClick))
   restartBtn.addEventListener('click', handleRestart)
 })()
-
-module.exports = { gameState }
---
+++ script.test.js
@@ -0,0 +1,5 @@
+describe('gamestate', function () {
+  it('gamestate len', function () {
+    chai.expect(gameState.length).to.equal(9)
+  })
+})
